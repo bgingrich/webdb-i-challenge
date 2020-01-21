cc09683609bef261d7c3bce50eb795fcc76bc05b
Billy Gingrich <whgingrich@gmail.com>
2020-01-21 11:55:55 -0500

    knex mvp complete

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,12 @@
+// Update with your config settings.
+
 module.exports = {
+
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/budget.db3',
+      filename: './dev.sqlite3'
     },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    }
-  },
+    useNullAsDefault: true
+  }
 };
